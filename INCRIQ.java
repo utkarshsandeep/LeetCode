baede54a5cf490ad3dd3f03922bcf54d06568636
Utkarsh Sandeep Singh <500053648@stu.upes.ac.in>
2022-03-27 11:03:25 +0530

    IQ Test

---
+++ INCRIQ.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int X = Integer.parseInt(br.readLine());
+		if (X<163)
+		System.out.println("NO");
+		else
+		System.out.println("YES");
+	}
+}
